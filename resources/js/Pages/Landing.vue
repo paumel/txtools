7de0bb6d223c9de7b8579809a94c48e76540c045
Paulius Melaika <pmpauliusm@gmail.com>
2022-09-24 15:15:34 +0300

    Home page

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -10,34 +10,31 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome"/>
+    <Head :title="$page.props.appName"/>
 
     <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-                  class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard
-            </Link>
+        class="relative flex flex-col items-top justify-start min-h-screen bg-gradient-to-r from-violet-100 to-purple-100 sm:items-center sm:pt-0">
 
-            <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
-
-                <Link v-if="canRegister" :href="route('register')"
-                      class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register
-                </Link>
-            </template>
+        <div class="flex w-full justify-center items-center h-32 md:h-48 lg:h-80">
+            <h1 class="text-6xl font-bold text-violet-800">{{ $page.props.appName }}</h1>
         </div>
 
-        <div class="flex ">
-            <div>
-                <Link :href="route('counter')" class="text-sm text-gray-700 dark:text-gray-500 underline">Counter</Link>
-            </div>
-            <div>
+        <div class="flex flex-wrap w-full justify-center">
+            <Link :href="route('case-converter')"
+                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-20 md:py-30 lg:py-40 bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-300 rounded-3xl">
+                <p class="text-5xl font-medium text-violet-800 dark:text-gray-500">
+                    Counter
+                </p>
+            </Link>
 
-                <Link :href="route('case-converter')" class="text-sm text-gray-700 dark:text-gray-500 underline">
+            <Link :href="route('case-converter')"
+                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-20 md:py-30 lg:py-40 bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-300 rounded-3xl">
+
+                <p class="text-5xl font-medium text-violet-800 dark:text-gray-500">
                     Case converter
-                </Link>
-            </div>
+                </p>
+            </Link>
+
         </div>
 
     </div>
