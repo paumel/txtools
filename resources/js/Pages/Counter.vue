380fa2781dd4519ad40b74a4982f5ec09c8bf1e2
Paulius Melaika <pmpauliusm@gmail.com>
2022-10-27 21:30:09 +0300

    color change

--- resources/js/Pages/Counter.vue
+++ resources/js/Pages/Counter.vue
@@ -30,52 +30,53 @@ function submit(type) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </Head>
 
-    <div class="flex justify-center bg-violet-500 space-x-4">
+    <div class="flex justify-center bg-slate-500 space-x-4">
         <Link :href="route('counter')"
-              class="m-2 w-full md:w-1/3 lg:w-1/6 text-center bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-700 rounded-xl">
-            <p class="text-xl md:text-2xl font-medium text-violet-800 dark:text-gray-500">
+              class="m-2 w-full md:w-1/3 lg:w-1/6 text-center bg-slate-200 hover:bg-slate-300 active:bg-slate-400 border-solid border-2 border-slate-700 rounded-xl">
+            <p class="text-xl md:text-2xl font-medium text-slate-800 dark:text-gray-500">
                 Counter
             </p>
         </Link>
 
         <Link :href="route('case-converter')"
-              class="m-2 w-full md:w-1/3 lg:w-1/6 text-center bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-500 rounded-xl">
-            <p class="text-xl md:text-2xl font-medium text-violet-800 dark:text-gray-500">
+              class="m-2 w-full md:w-1/3 lg:w-1/6 text-center bg-slate-200 hover:bg-slate-300 active:bg-slate-400 border-solid border-2 border-slate-500 rounded-xl">
+            <p class="text-xl md:text-2xl font-medium text-slate-800 dark:text-gray-500">
                 Case converter
             </p>
         </Link>
     </div>
 
     <div
-        class="relative flex flex-col items-top justify-start min-h-screen bg-gradient-to-r from-violet-100 to-purple-100 sm:items-center sm:pt-0">
+        class="relative flex flex-col items-top justify-start min-h-screen bg-gradient-to-r from-slate-100 to-purple-100 sm:items-center sm:pt-0">
 
         <div class="flex w-full justify-center items-center h-32 md:h-48 lg:h-80">
-            <h1 class="text-4xl md:text-6xl font-bold text-violet-800">Counter</h1>
+            <h1 class="text-4xl md:text-6xl font-bold text-slate-800">Counter</h1>
         </div>
 
         <div class="flex flex-col w-full justify-center items-center">
             <div class="w-full text-center my-2 md:my-4">
                 <textarea
-                    class="w-11/12 md:w-1/2 lg:w-1/3 text-violet-800 border-solid border-2 border-violet-300 rounded-xl"
+                    class="w-11/12 md:w-1/2 lg:w-1/3 text-slate-800 border-solid border-2 border-slate-300 rounded-xl"
                     name="string" id=""
                     cols="60"
                     rows="10" placeholder="Write your text here..." v-model="form.string"></textarea>
             </div>
             <div class="flex flex-wrap space-x-4">
                 <button @click="submit(type)"
-                        class="mx-1 md:mx-2 my-1 p-4 bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-300 rounded-xl text-sm md:text-xl text-violet-800"
-                        :class="{ 'bg-violet-300': currentType === type, 'bg-violet-200': currentType !== type }"
+                        class="mx-1 md:mx-2 my-1 p-4 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 border-solid border-2 border-slate-300 rounded-xl text-sm md:text-xl text-slate-800"
+                        :class="{ 'bg-slate-300': currentType === type, 'bg-slate-200': currentType !== type }"
                         v-for="type in types">
                     {{ type }}
                 </button>
             </div>
         </div>
         <div class="flex justify-center items-center mt-4 space-x-2 md:space-x-4" v-if="result">
-            <div class="bg-violet-300 text-violet-800 border-solid border-2 border-violet-300 rounded-xl">
-                <p class="text-xl font-medium text-violet-900 dark:text-gray-500 p-4">Result:</p>
+            <div class="bg-slate-300 text-slate-800 border-solid border-2 border-slate-300 rounded-xl">
+                <p class="text-xl font-medium text-slate-900 dark:text-gray-500 p-4">Result:</p>
             </div>
-            <div class="bg-violet-300 text-violet-800 border-solid border-2 border-violet-300 rounded-xl" v-if="result">
-                <p class="text-xl font-bold text-violet-900 dark:text-gray-500 p-4"
+            <div class="bg-slate-300 text-slate-800 border-solid border-2 border-slate-300 rounded-xl"
+                 v-if="result">
+                <p class="text-xl font-bold text-slate-900 dark:text-gray-500 p-4"
                 >{{ result.value }}</p>
             </div>
         </div>
--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -19,35 +19,35 @@ defineProps({
     </Head>
 
     <div
-        class="relative flex flex-col items-top justify-start min-h-screen bg-gradient-to-r from-violet-100 to-purple-100 sm:items-center sm:pt-0">
+        class="relative flex flex-col items-top justify-start min-h-screen bg-gradient-to-r from-slate-100 to-purple-100 sm:items-center sm:pt-0">
 
         <div class="flex w-full justify-center items-center h-32 md:h-48 lg:h-80">
-            <h1 class="text-6xl font-bold text-violet-800">{{ $page.props.appName }}</h1>
+            <h1 class="text-6xl font-bold text-slate-800">{{ $page.props.appName }}</h1>
         </div>
         <div class="text-left px-3 md:px-0 md:text-center mb-5">
-            <p class="text-md md:text-xl font-medium text-violet-800">This is a free, online tool to help you with your
+            <p class="text-md md:text-xl font-medium text-slate-800">This is a free, online tool to help you with your
                 texts,
                 titles or messages.</p>
-            <p class="text-md md:text-xl font-medium text-violet-800">You can choose Case Converter if you want to
+            <p class="text-md md:text-xl font-medium text-slate-800">You can choose Case Converter if you want to
                 transform your
                 text to any shape you like.</p>
-            <p class="text-md md:text-xl font-medium text-violet-800">If you want to know how many characters, words or
+            <p class="text-md md:text-xl font-medium text-slate-800">If you want to know how many characters, words or
                 letters your
                 text contains, you can choose Counter</p>
         </div>
 
         <div class="flex flex-wrap w-full justify-center">
             <Link :href="route('counter')"
-                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-10 md:py-30 lg:py-40 bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-300 rounded-3xl">
-                <p class="text-3xl md:text-5xl font-medium text-violet-800 dark:text-gray-500">
+                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-10 md:py-30 lg:py-40 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 border-solid border-2 border-slate-300 rounded-3xl">
+                <p class="text-3xl md:text-5xl font-medium text-slate-800 dark:text-gray-500">
                     Counter
                 </p>
             </Link>
 
             <Link :href="route('case-converter')"
-                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-10 md:py-30 lg:py-40 bg-violet-200 hover:bg-violet-300 active:bg-violet-400 border-solid border-2 border-violet-300 rounded-3xl">
+                  class="m-2 w-full md:w-1/3 lg:w-1/4 text-center py-10 md:py-30 lg:py-40 bg-slate-200 hover:bg-slate-300 active:bg-slate-400 border-solid border-2 border-slate-300 rounded-3xl">
 
-                <p class="text-3xl md:text-5xl font-medium text-violet-800 dark:text-gray-500">
+                <p class="text-3xl md:text-5xl font-medium text-slate-800 dark:text-gray-500">
                     Case converter
                 </p>
             </Link>
